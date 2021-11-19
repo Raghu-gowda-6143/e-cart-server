d0c6896e9f1988fa11c46d43570adba0e55e46c1
Raghu-gowda-6143 <raghugowda0144@gmail.com>
2021-11-19 16:34:55 +0530

    stripe

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors());
+app.use(cors({origin:"https://raghu-e-cart.netlify.app"|| "http://localhost:3000"}));
 
 
 //connecting database
@@ -17,7 +17,7 @@ require("./config/db");
 
 const errorHandler = require("./middleware/error");
 
-router.get("/", () => {
+router.get("/", (req, res) => {
     res.render("welcome");
 });
 
