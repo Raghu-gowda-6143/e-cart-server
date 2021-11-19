38166b235c112b5c238fd9499fed2c4795908f27
Raghu-gowda-6143 <raghugowda0144@gmail.com>
2021-11-19 15:30:23 +0530

    stripe

--- controllers/payment-controller.js
+++ controllers/payment-controller.js
@@ -1,82 +1,34 @@
-const paypal = require('paypal-rest-sdk');
+const stripe = require('stripe')(process.env.SECRETE_KEY) 
 
-const client = process.env.CLIENT_URI
+exports.payment=  async (req, res)=>{ 
 
-paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': process.env.PAYPAL_CLIENT_ID,
-    'client_secret': process.env.PAYPAL_CLIENT_SECRET_ID
-});
+    // Moreover you can take more details from user 
+    // like Address, Name, etc from form 
+    stripe.customers.create({ 
+        email: req.body.stripeEmail, 
+        source: req.body.stripeToken, 
+        name: 'Gautam Sharma', 
+        address: { 
+            line1: 'TC 9/4 Old MES colony', 
+            postal_code: '110092', 
+            city: 'New Delhi', 
+            state: 'Delhi', 
+            country: 'India', 
+        } 
+    }) 
+    .then((customer) => { 
 
-
-
-exports.payment = async (req, res) => {
-    const create_payment_json = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
-        },
-        "redirect_urls": {
-            "return_url": `${client}/success`,
-            "cancel_url": `${client}/cancel`
-        },
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": req.body.name,
-                    "sku": "001",
-                    "price": req.body.price,
-                    "currency": "USD",
-                    "quantity": 1
-                }]
-            },
-            "amount": {
-                "currency": "USD",
-                "total": req.body.price
-            },
-            "description": "This is the payment description."
-        }]
-    };
-
-    paypal.payment.create(create_payment_json, (error, payment) => {
-        if (error) {
-            throw error;
-        } else {
-            for (let i = 0; i < payment.links.length; i++) {
-                if (payment.links[i].rel === 'approval_url') {
-                    res.redirect(payment.links[i].href);
-                }
-            }
-        }
-    });
-
-};
-
-exports.success = async (req, res) => {
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-    const total = req.query.total
-
-    const execute_payment_json = {
-        "payer_id": payerId,
-        "transactions": [{
-            "amount": {
-                "currency": "USD",
-                "total": total
-            }
-        }]
-    };
-
-    paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
-        if (error) {
-            console.log(error.response);
-            throw error;
-        } else {
-            console.log(JSON.stringify(payment));
-            res.send('Success');
-        }
-    });
-};
-
-
-exports.cancel = (req, res) => res.send('Cancelled');
+        return stripe.charges.create({ 
+            amount: 7000,    // Charing Rs 25 
+            description: 'Web Development Product', 
+            currency: 'USD', 
+            customer: customer.id 
+        }); 
+    }) 
+    .then((charge) => { 
+        res.send("Success") // If no error occurs 
+    }) 
+    .catch((err) => { 
+        res.send(err)    // If some error occurs 
+    }); 
+} 
--- routers/payment-router.js
+++ routers/payment-router.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { payment, success, cancel } = require('../controllers/payment-controller');
+const { payment } = require('../controllers/payment-controller');
 
 const auth = require("../middleware/auth");
 
 
 
 router.post("/pay", auth, payment);
-router.get("/success", auth, success);
-router.get("/cancel", auth, cancel);
+
 
 
 module.exports = router;
